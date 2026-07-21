47ca5f30c359cf1c89d79cb8dff2264a723fb92e
Kanhaiya <ikanhaiya2001@gmail.com>
2026-07-22 03:15:46 +0530

    Asynchronizationis achieved with waiting queue (logQ) and workerThread, producer consumer architecture is implemented

--- Projects/customeStringClass/threadSafeLogger/src/main.cpp
+++ Projects/customeStringClass/threadSafeLogger/src/main.cpp
@@ -12,18 +12,35 @@ enum class severity{
     Count
 };
 
+
 enum class logSaveChoice{   // tells where to output log in logfile, in console or in both, 
     LOGFILE,
     CONSOLE,
     BOTH
 };
 
+
+struct LogEntry {
+    chrono::system_clock::time_point timestamp;
+    severity level;
+    string message;
+};
+
+
 class Logger{
 
-    ofstream logfile;
-    mutex writemtx;
+    ofstream logfile;       
+    mutex writemtx;    
+    mutex Qmtx;
     severity minSeverity;
     logSaveChoice logChoice;
+    queue<LogEntry> logQ;             // waiting queue 
+    condition_variable cv;
+
+    thread workerThread;
+    bool stopReq = false;
+
+
 
     public:
         Logger(const string& AppName, severity mini,logSaveChoice c): logfile(AppName + ".log", std::ios::app), minSeverity(mini), logChoice(c){
@@ -31,7 +48,20 @@ class Logger{
             if (!logfile) {
                 throw runtime_error("Failed to open log file");
             }
-            // log("application started", level);
+
+            workerThread = thread(&Logger::processLog, this);
+            
+        }
+
+        ~Logger(){
+
+            {
+                lock_guard<mutex> lock(Qmtx);  
+                stopReq = true;                 // this is to tell the worker thread that write remaining logs (no new data will come) and then exit
+
+            }
+
+            workerThread.join();                // waiting for worker thread to finish.  
         }
 
         string toString(const severity& level){
@@ -48,40 +78,72 @@ class Logger{
             return "UNKNOWN";
         }
 
-        auto getTime(){
-            auto now = chrono::system_clock::now();
+        auto getTime(chrono::system_clock::time_point timestamp){
 
-            time_t currentTime = chrono::system_clock::to_time_t(now);
+            time_t currentTime = chrono::system_clock::to_time_t(timestamp);
             
         
             return put_time(localtime(&currentTime),
                         "%Y-%m-%d %H:%M:%S");
         }
 
-        void log(string msg, severity level){
 
-            if(static_cast<int>(level) < static_cast<int>(minSeverity)) return;
+        void log(string msg, severity level){                    // log is producer
 
-            auto t = getTime();
-            string lev = toString(level);
+            if(static_cast<int>(level) < static_cast<int>(minSeverity)) return;  // if severity is less then minimum severity then do not put it in waiting queue, hence not in logfile 
 
-            lock_guard<mutex> lock(writemtx);
+            LogEntry entry;                                          
+            entry.level=level;                                                                                       
+            entry.message = msg;                                      // here we are getting all the log information which is to be pushed in waiting queue(logQ);
+            entry.timestamp =  chrono::system_clock::now();             
+            
+            lock_guard<mutex> lock(Qmtx);              // locking the mutex of queue
+            logQ.push(move(entry));                   //entry wont be used further so move it inside logQ 
+            
+        }
+
+    private: 
+        void processLog(){                 // process log is consumer. 
+
+            while(true){         // this will keep running once the logger object is created. 
+                LogEntry entry;
+                {
+                lock_guard<mutex> lock(Qmtx);   // lock the Qmtx we are going to fetch the data from waiting to put it inside Logfile.
+
+                    if(stopReq && logQ.empty()){ 
+                        break;                            // this is when main thread reaches the destructor and not data will come so if log is not empty put all data in logfile then exit from worker thread. 
+                    }
+                    
+                    if(logQ.empty()){
+                    continue;                        // if Q is empty wait log data may come in future(its busy waiting);
+                    }
+                    entry = logQ.front();           // getting data from Q;
+                    logQ.pop();                  
+                    
+                }
+                
+
+            auto t = getTime(entry.timestamp);
+            string lev = toString(entry.level);
+
+
+
+            lock_guard<mutex> lock(writemtx);                           // writing the data in Logfile and terminal so making the write process thread safe.
             if(logChoice == logSaveChoice::LOGFILE){
-             logfile<<t<<": ["<<lev<<"]"<<": "<<msg<<"\n";  
+             logfile<<t<<": ["<<lev<<"]"<<": "<<entry.message<<"\n";  
              // this 
             }
             else if(logChoice == logSaveChoice::CONSOLE){
                           
-                cout<<t<<": ["<<lev<<"]"<<": "<<msg<<"\n";  // this line is atomic 
+                cout<<t<<": ["<<lev<<"]"<<": "<<entry.message<<"\n";  
             }
             else{
-                logfile<<t<<": ["<<lev<<"]"<<": "<<msg<<"\n";   // this whole block is atomic 
-                cout<<t<<": ["<<lev<<"]"<<": "<<msg<<"\n";
+                logfile<<t<<": ["<<lev<<"]"<<": "<<entry.message<<"\n";  
+                cout<<t<<": ["<<lev<<"]"<<": "<<entry.message<<"\n";
+            } 
+
             }
-            
-            
-            
-            
+        
         }
 
 };
@@ -92,6 +154,5 @@ int main(){
     Logger logger("BankingApp", severity::WARNING,logSaveChoice::BOTH);
     logger.log("Application Started", severity::FATAL);
 
-    
     return 0;
 }
